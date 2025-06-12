a8b05c8454b594108a7ec325e9684e26afda0886
yevge <yevgeniysarancha@gmail.com>
2025-06-12 10:01:17 +0200

    Richest Customer Wealth

---
+++ src/tasks/array/easy/RichestCustomerWealth.py
@@ -0,0 +1,15 @@
+
+
+class Solution:
+    @staticmethod
+    def maximumWealth(accounts):
+        max_wealth = 0
+        for customer in accounts:
+            current_wealth = sum(customer)
+            if current_wealth > max_wealth:
+                max_wealth = current_wealth
+        return max_wealth
+
+
+accounts = [[1, 3, 2], [2, 1, 4]]
+print(Solution.maximumWealth(accounts))
