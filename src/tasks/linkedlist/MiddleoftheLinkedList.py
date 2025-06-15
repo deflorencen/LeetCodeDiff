ea05e1d408da02a4009ed62b28db0582a33233b9
yevge <yevgeniysarancha@gmail.com>
2025-06-15 15:07:27 +0200

    Middle of the Linked List

---
+++ src/tasks/linkedlist/MiddleoftheLinkedList.py
@@ -0,0 +1,13 @@
+
+
+class Solution:
+    @staticmethod
+    def middleNode(head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
